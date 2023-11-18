f6235fde20148b591de159359b196909210f2288
Schareazar <kukawski.marcin@gmail.com>
2023-11-18 22:45:49 +0100

    1st selenium test

---
+++ src/test/java/Tests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tests {
+    WebDriver driver;
+    @BeforeEach
+    public void setup()
+    {
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    public void teardown()
+    {
+        driver.quit();
+    }
+    @Test
+    public void exampleTest()
+    {
+        String url = "http://localhost:8080/";
+        driver.get(url);
+        Assertions.assertEquals(url, driver.getCurrentUrl(), "Url different than expected");
+    }
+
+}
