47a87e51c8bb2aae75baf0a5beedb6518ea26f27
Schareazar <kukawski.marcin@gmail.com>
2023-11-22 08:44:01 +0100

    finding 1st elements

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,9 +1,8 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import java.util.List;
 
 public class Tests {
     WebDriver driver;
@@ -18,11 +17,23 @@ public class Tests {
         driver.quit();
     }
     @Test
-    public void exampleTest()
+    public void urlTest()
     {
         String url = "http://localhost:8080/";
         driver.get(url);
-        Assertions.assertEquals(url, driver.getCurrentUrl(), "Url different than expected");
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(url, driver.getCurrentUrl(), "Url different than expected"),
+                () -> Assertions.assertEquals("Test App - Just another WordPress site", driver.getTitle(), "Site title different than expected")
+        );
     }
+    @Test
+    public void firstFindBy()
+    {
+        driver.get("http://localhost:8080/");
+        WebElement searchField = driver.findElement(By.id("wc-block-search__input-1"));
+        searchField.click();
+        List<WebElement> addToCart = driver.findElements(By.className("add_to_cart_button"));
+    }
+
 
 }
