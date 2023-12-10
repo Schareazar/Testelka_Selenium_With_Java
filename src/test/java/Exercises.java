467b7bc9a1b2d7cdab400e62ad88dda1282318fb
Schareazar <kukawski.marcin@gmail.com>
2023-12-10 15:34:46 +0100

    Exercises 3

--- src/test/java/Exercises.java
+++ src/test/java/Exercises.java
@@ -56,6 +56,35 @@ public class Exercises {
                 "Town-square homepage was not displayed, login failed");
     }
 
+    @Test
+    public void exercise3a()
+    {
+        driver.get("http://localhost:8065/login/");
+        wait.until(driver -> driver.findElement(By.className("get-app__continue"))).click();
+        wait.until(driver -> driver.findElement(By.name("loginId"))).sendKeys("test");
+        driver.findElement(By.name("password-input")).sendKeys("marcin");
+        driver.findElement(By.id("saveSetting")).click();
+        driver.get("http://localhost:8065/admin_console/user_management/permissions/system_scheme");
+        WebElement saveButton = wait.until(ExpectedConditions.presenceOfElementLocated((By.id("saveSetting"))));
+        Assertions.assertFalse(saveButton.isEnabled(), "Save button is not disabled by default");
+    }
+
+    @Test
+    public void exercise3b()
+    {
+        driver.get("http://localhost:3000/new");
+        WebElement title = driver.findElement(By.id("title"));
+        WebElement location = driver.findElement(By.id("location"));
+        WebElement description = driver.findElement(By.id("description"));
+        Assertions.assertAll
+                (
+                        () -> Assertions.assertEquals("Monthly Meetup", title.getDomAttribute("placeholder")),
+                        () -> Assertions.assertEquals("Joe's Coffee Shop", location.getDomAttribute("placeholder")),
+                        () -> Assertions.assertEquals("Hey everyone, please choose the dates that work for you!",
+                                description.getDomAttribute("placeholder"))
+                );
+
+    }
 
 
 }
