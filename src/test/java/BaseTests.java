6ab9b445b27e3108c2916cb7d6ace911974801cd
Schareazar <kukawski.marcin@gmail.com>
2023-12-17 14:41:31 +0100

    Headless

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.*;
 import java.time.Duration;
 
@@ -12,7 +15,17 @@ public class BaseTests {
     @BeforeEach
     public void setup()
     {
-        driver = new ChromeDriver();
+//        FirefoxOptions ffOptions = new FirefoxOptions();
+//        ffOptions.addArguments("-headless");
+//        driver = new FirefoxDriver(ffOptions);
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless=new");
+//        driver = new ChromeDriver(chromeOptions);
+
+        EdgeOptions ieOptions = new EdgeOptions();
+        ieOptions.addArguments("--headless=new");
+        driver = new EdgeDriver(ieOptions);
+
         // implicitWait shouldn't be used when webDriverWait is used
         // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
