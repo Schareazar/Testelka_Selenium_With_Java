5e74e1c6162ffd9a4c6950ff4640aca3c113e13c
Schareazar <kukawski.marcin@gmail.com>
2024-02-25 14:28:43 +0100

    ShadowDOM and javascript executor

--- src/test/java/Course/BaseTests.java
+++ src/test/java/Course/BaseTests.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -18,14 +20,15 @@ public class BaseTests {
     @BeforeEach
     public void setup()
     {
-/*
-        FirefoxOptions ffOptions = new FirefoxOptions();
-        ffOptions.addArguments("-headless");
-        driver = new FirefoxDriver(ffOptions);
-        EdgeOptions ieOptions = new EdgeOptions();
-        ieOptions.addArguments("--headless=new");
-        driver = new EdgeDriver(ieOptions);
-*/
+
+//        FirefoxOptions ffOptions = new FirefoxOptions();
+//       // ffOptions.addArguments("-headless");
+//        driver = new FirefoxDriver(ffOptions);
+
+//        EdgeOptions ieOptions = new EdgeOptions();
+//        ieOptions.addArguments("--headless=new");
+//        driver = new EdgeDriver(ieOptions);
+
         ChromeOptions chromeOptions = new ChromeOptions();
         //chromeOptions.addArguments("--headless=new");
         driver = new ChromeDriver(chromeOptions);
---
+++ src/test/java/Course/ShadowDOMTests.java
@@ -0,0 +1,46 @@
+package Course;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ShadowDOMTests extends BaseTests{
+
+    @Test
+    public void shadowDOMChromium()
+    {
+        String input = "Test";
+        driver.get("https://fakestore.testelka.pl/shadow-dom-w-selenium/");
+        WebElement shadowHost = driver.findElement(By.cssSelector("#host"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+        shadowRoot.findElement(By.cssSelector("#input")).sendKeys(input);
+        shadowRoot.findElement(By.cssSelector("button")).click();
+
+        Assertions.assertEquals("Wprowadzony tekst: " + input, shadowRoot.findElement(By.cssSelector("#output")).getText(),
+                "Displayed text different than provided input");
+    }
+
+    @Test
+    public void shadowDOMFirefox()
+    {
+        String input = "Test";
+        driver.get("https://fakestore.testelka.pl/shadow-dom-w-selenium/");
+        WebElement shadowHost = driver.findElement(By.cssSelector("#host"));
+        //javascript executed in browser
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        List<WebElement> shadowContent = (List<WebElement>) jsExecutor.executeScript
+                ("return arguments[0].shadowRoot.children", shadowHost);
+        WebElement shadowRoot = shadowContent.getFirst();
+
+        shadowRoot.findElement(By.cssSelector("#input")).sendKeys(input);
+        shadowRoot.findElement(By.cssSelector("button")).click();
+
+        Assertions.assertEquals("Wprowadzony tekst: " + input, shadowRoot.findElement(By.cssSelector("#output")).getText(),
+                "Displayed text different than provided input");
+    }
+}
